7d3a22c8f45f2a3a224b26a1962aa6adf4def2d9
anantharamanan <75565881+anantharamanan@users.noreply.github.com>
2022-10-15 15:20:19 +0530

    Add files via upload

---
+++ Week2/ecea_week2_fulladd.v
@@ -0,0 +1,8 @@
+
+module fulladd(a,b,cin,sum,cout);
+  input a,b,cin;
+  output sum,cout;
+  
+  assign sum=(a^b^cin);
+  assign cout=(a&b)|(b&cin)|(cin&a);
+endmodule
---
+++ Week2/ecea_week2_fulladd_tb.v
@@ -0,0 +1,19 @@
+
+module full_addtb;
+reg a,b,cin;
+wire sum,cout;
+fulladd  DUT(a,b,cin,sum,cout);
+ initial begin
+   $monitor($time,"a=%b b=%b cin=%b sum=%b cout=%b",a,b,cin,sum,cout);
+   $dumpvars(0);
+   $dumpfile("adder_res.vcd");
+   #1 a=0;b=0;cin=0;
+   #1 cin=1;
+   #1 a=0;b=1;cin=0;
+   #1 cin=1;
+   #1 a=1;b=0;cin=0;
+   #1 cin=1;
+   #1 a=1;b=1;cin=0;
+   #1 cin=1;
+ end
+endmodule
